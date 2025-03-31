b362f57292d4c6e0818b4809eb49b2436d11ddea
Gustaf Gren <6107181+skogsgren@users.noreply.github.com>
2025-03-31 09:04:17 +0200

    add data; update tests

--- tests_generic/test_41_perceptron.py
+++ tests_generic/test_41_perceptron.py
@@ -3,16 +3,16 @@ from pathlib import Path
 from tinlp.clf import PerceptronMorphClassifier
 from tinlp.utils.data import get_data_split
 
-TRAIN_DATA = Path("./data/unimorph/swedish-train-high")
-TEST_DATA = Path("./data/unimorph/swedish-test")
+TRAIN_DATA = Path("./data/unimorph/swe-train")
+TEST_DATA = Path("./data/unimorph/swe-test")
 
 
 def test_perceptron_morphological_clf():
+    print()
     clf = PerceptronMorphClassifier(
         params={"metric": "accuracy", "epochs": 5, "affix_len": 5}
     )
     clf.fit(*get_data_split(TRAIN_DATA, data_type="unimorph"))
     score = clf.eval(*get_data_split(TEST_DATA, data_type="unimorph"))
     print(score)
-    print(clf.weights.most_common(15))
     assert score > 0.5
