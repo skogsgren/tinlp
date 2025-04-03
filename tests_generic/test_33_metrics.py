53763f64f539a758ab10caf033e2e8ef67176526
Gustaf Gren <6107181+skogsgren@users.noreply.github.com>
2025-04-03 17:55:46 +0200

    fix test cases

--- tests_generic/test_33_metrics.py
+++ tests_generic/test_33_metrics.py
@@ -1,16 +1,15 @@
 from tinlp.eval import Metric
 
+Y = [0, 1, 2, 2, 0, 1, 2, 0, 1, 2]
+Y_HAT = [0, 0, 2, 2, 0, 2, 1, 0, 1, 2]
 
-Y = [1, 1, 1, 0, 0]
-Y_HAT = [1, 0, 1, 0, 1]
-
-ACC = 0.6000
-F1 = 0.5833
+ACC = 0.70
+F1 = 0.67
 
 
 def test_accuracy_metric():
-    assert round(Metric("accuracy")(Y, Y_HAT), 4) == ACC
+    assert round(Metric("accuracy")(Y, Y_HAT), 2) == ACC
 
 
 def test_f1_score_metric():
-    assert round(Metric("f1")(Y, Y_HAT), 4) == F1
+    assert round(Metric("f1")(Y, Y_HAT), 2) == F1
