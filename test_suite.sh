ff0ada528645b151cc9892fa3eb268b84b927974
Gustaf Gren <6107181+skogsgren@users.noreply.github.com>
2025-04-09 11:37:32 +0200

    remove redundant echo statements

--- test_suite.sh
+++ test_suite.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
-echo "testing generic functionality"
 cd tests_generic/ || exit
 python3 -m pytest -v
 cd .. || exit
 
-echo "testing lab specific metrics"
 cd tests_labs/ || exit
 python3 -m pytest -v
 cd .. || exit
