f743ec94cf3ca749f234c5105edcd9f9f062a394
Gustaf Gren <6107181+skogsgren@users.noreply.github.com>
2025-05-08 13:06:06 +0200

    fix incorrect file ending

--- compress_repo.sh
+++ compress_repo.sh
@@ -3,7 +3,8 @@ set -e
 TMPDIR=$(mktemp -d)
 TMPPATH=$(dirname "$TMPDIR")
 NAME=$(basename "$TMPDIR")
-OUTFILE="./repo.tar.xz"
+OUTFILE="./repo.tar.gz"
+
 [ -f "$OUTFILE" ] && trash-put "$OUTFILE"
 git ls-files --exclude-standard | xargs -I {} cp --parents {} "$TMPDIR"
 git ls-files --exclude-standard -o | xargs -I {} cp --parents {} "$TMPDIR"
